b2467de188025e61cb32c3130fdf0045523ab306
ryudama-private <my_com_address@yahoo.co.jp>
2026-04-12 23:51:05 +0900

    フロント編集自動反映

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,12 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    watch: {
+      usePolling: true,
+      interval: 300,
+    },
+  },
 });
