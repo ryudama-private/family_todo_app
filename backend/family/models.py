c7628dc61a5ec0a50910e39fa377d619740ff368
ryudama-private <my_com_address@yahoo.co.jp>
2026-04-26 14:05:54 +0900

    taskテーブル作成

--- backend/family/models.py
+++ backend/family/models.py
@@ -14,3 +14,21 @@ class Family(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+# Taskモデル
+class Task(models.Model):
+    title = models.CharField(max_length=255, verbose_name="やること")
+    creator = models.ForeignKey('Family', related_name='created_tasks', on_delete=models.CASCADE, verbose_name="作った人")
+    assignee = models.ForeignKey('Family', related_name='assigned_tasks', on_delete=models.CASCADE, verbose_name="やる人")
+    due_date = models.DateTimeField(verbose_name="期限")
+    status = models.CharField(max_length=50, verbose_name="進行状況")
+    alarm_minutes = models.IntegerField(verbose_name="アラーム", null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="作成日時")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="更新日時")
+
+    class Meta:
+        db_table = "task"
+
+    def __str__(self):
+        return self.title
